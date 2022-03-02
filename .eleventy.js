575a6ce90e43184a096d73ab5a5aef3e011eafab
jerobros <brosgoljerome@yahoo.fr>
2022-03-02 23:01:07 +0100

    installation environment

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+
+/* Config file */
+module.exports = function (eleventyConfig) {
+
+  // Ce dossier est cloné lors du build
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    
+    }
+  }
+};
